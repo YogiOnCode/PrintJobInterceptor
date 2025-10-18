89d24b5d9565ae2c42cf7f577f0a80b969e5a111
yogiii1762 <yogi.yogeswaran17@gmail.com>
2025-10-18 16:55:40 +0200

    PresenterClass added with functionalities (not Tested)

---
+++ Presentation/MainformPresenter.cs
@@ -0,0 +1,38 @@
+﻿using PrintJobInterceptor.Core.Interfaces;
+using PrintJobInterceptor.Core.Models;
+using PrintJobInterceptor.UI.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintJobInterceptor.Presentation
+{
+    public class MainFormPresenter
+    {
+        private readonly IMainFormView _view;
+        private readonly IPrintJobService _printJobService;
+        private readonly List<PrintJob> _jobs;
+
+        public MainFormPresenter(IMainFormView view, IPrintJobService printJobService)
+        {
+            _view = view;
+            _printJobService = printJobService;
+            _jobs = new List<PrintJob>();
+         
+            _printJobService.JobSpooling += OnJobSpooling;
+        }
+
+        private void OnJobSpooling(PrintJob newJob)
+        {
+            
+            _jobs.Add(newJob);
+
+        
+            _view.DisplayJobs(_jobs.ToList());
+        }
+
+        public void Start()
+        {
+            _printJobService.StartMonitoring();
+        }
+    }
+}
